9d338d6c97083e207255617d7714b91e716fc83b
edu <edward1738@gmail.com>
2024-09-20 11:10:50 -0500

    starting

---
+++ src/main/java/com/ecacho/c8queryapi/C8queryapiApplication.java
@@ -0,0 +1,13 @@
+package com.ecacho.c8queryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C8queryapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(C8queryapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ecacho/c8queryapi/C8queryapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecacho.c8queryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C8queryapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
